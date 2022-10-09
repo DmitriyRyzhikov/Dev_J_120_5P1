f82ad149d580042957e33b3f4f1d1cfbfcd46524
dmitr <dmitr@PapaLucky>
2022-10-10 01:42:40 +0300

    Почти Готово и передано на проверку

--- Dev_J_120_5P1/src/Dev_J_120/MainFrame.java
+++ Dev_J_120_5P1/src/Dev_J_120/MainFrame.java
@@ -6,16 +6,13 @@ import java.awt.Dimension;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
@@ -29,37 +26,24 @@ import javax.swing.tree.TreePath;
 
 public class MainFrame extends JFrame{
     
+    private final MainFrame frame;
     private JTree tree;
     private JTextArea textArea;
-    private JSplitPane splitPane;
     private MyTreeModel treeModel;
 
-    public MainFrame() {
+    public MainFrame() 
+    {   frame = this;
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setBounds(400, 230, 1000, 700);
-        init();
-        addMenu();       
+        init();  
     }
     
     private void init(){
-        JPanel panel = new JPanel(new BorderLayout(3, 3));
-        textArea = new JTextArea();
         treeModel = new MyTreeModel(System.getProperty("user.dir"));
         setTitle(treeModel.getRoot().toString()); 
         tree = new JTree(treeModel); 
-        tree.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                TreePath seldPath = tree.getSelectionPath(); 
-                if(e.getKeyCode() == KeyEvent.VK_ENTER)
-                    if(tree.isCollapsed(seldPath)) 
-                       tree.expandPath(seldPath);
-                    else
-                       tree.collapsePath(seldPath); 
-            }
-        });
-        setSelectionListener();
-        tree.setCellRenderer(new FileTreeCellRenderer());
+        tree.setCellRenderer(new FileTreeCellRenderer());       
+        textArea = new JTextArea();
         JScrollPane treeScroll = new JScrollPane(tree);
         JScrollPane textScroll = new JScrollPane(textArea);
         Dimension dim = new Dimension(200, 500);
@@ -69,9 +53,13 @@ public class MainFrame extends JFrame{
         textScroll.setBorder(new EmptyBorder(5, 5, 5, 5));
         treeScroll.setBackground(Color.WHITE);
         textScroll.setBackground(Color.WHITE); 
-        splitPane = new JSplitPane(HORIZONTAL_SPLIT, treeScroll, textScroll);
+        JSplitPane splitPane = new JSplitPane(HORIZONTAL_SPLIT, treeScroll, textScroll);
+        JPanel panel = new JPanel(new BorderLayout(3, 3));
         panel.add(splitPane, BorderLayout.CENTER);
         add(panel);
+        setKeyListener();
+        setSelectionListener();
+        addMenu();
         Util.closeApp(this); 
     }
      private void addMenu(){
@@ -96,26 +84,63 @@ public class MainFrame extends JFrame{
         tree.addTreeSelectionListener(new TreeSelectionListener() {
         @Override
         public void valueChanged(TreeSelectionEvent event) {
+
             File file = (File) tree.getLastSelectedPathComponent();
-            if(file!=null && file.isFile())
+            if(file!=null && file.isDirectory())  
+                setTitle(file.toString());
+            
+            if(file!=null && file.isFile()) {
                try {
                    textArea.setText(fileReader(file));
                    textArea.setCaretPosition(0);
-            } catch (IOException ex) {
-                Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
-            }
+                   }
+               catch (IOException ex) 
+                   {
+                     JOptionPane.showMessageDialog
+            (frame, 
+            "An error occurred while trying to read the file.", 
+            "IOException error",
+	    JOptionPane.ERROR_MESSAGE);
+                   }
+                }
             else 
-               textArea.setText(""); 
+                 textArea.setText(""); 
                 }
         });
     }
-    public String fileReader(File file) throws FileNotFoundException, IOException, OutOfMemoryError{
+    public void setKeyListener(){
+        tree.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                TreePath selPath = tree.getSelectionPath(); 
+                if(e.getKeyCode() == KeyEvent.VK_ENTER)
+                    if(tree.isCollapsed(selPath)) 
+                       tree.expandPath(selPath);
+                    else
+                       tree.collapsePath(selPath); 
+            }
+        });    
+    }
+
+    public String fileReader(File file) throws IOException {
         FileReader fr = new FileReader(file); 
         StringBuilder sb = new StringBuilder();
+        try 
+           {
             char[] buf = new char[8192];
             int n;
-            while((n = fr.read(buf)) >= 0) {
-                sb.append(buf, 0, n);
+            while((n = fr.read(buf)) >= 0) 
+                 {
+                  sb.append(buf, 0, n);
+                 }
+            }
+        catch (OutOfMemoryError error) 
+            {
+             JOptionPane.showMessageDialog
+            (this, 
+            "Out Of Memory.", 
+            "OutOfMemoryError error",
+	    JOptionPane.ERROR_MESSAGE);
             }
         return sb.toString();
     }
